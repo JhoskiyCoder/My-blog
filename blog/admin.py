9e52305672606a41b5c440c3a7c497d173f72098
Рауф <rauf@MacBook-Pro-Rauf.local>
2023-09-13 17:25:41 +0600

    second commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# #git add .
+# git commit -m "first commit 
+# git ветка -M main 
+# git remote add origin https://github.com/JhoskiyCoder/My-blog.git
+#  git push -u origin maingit init
